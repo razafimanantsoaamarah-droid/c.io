590269720b723169b3ed23e67348954a720266aa
razafimanantsoaamarah-droid <razafimanantsoaamarah@gmail.com>
2026-01-24 16:18:51 +0000

    feat(isascii):Return True if all characters in the string are ASCII, False otherwise. ASCII characters have code points in the range U+0000-U+007F. Empty string is ASCII too.

--- srcs/ft_isascii.c
+++ srcs/ft_isascii.c
@@ -1,21 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/24 15:22:59 by codespace         #+#    #+#             */
-/*   Updated: 2026/01/24 15:54:05 by codespace        ###   ########.fr       */
+/*   Created: 2026/01/24 15:54:53 by codespace         #+#    #+#             */
+/*   Updated: 2026/01/24 16:15:19 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-int ft_isalpha(int charset)
-{
-    return ((charset >= 'a' && charset <= 'z') || (charset >= 'A' && charset <= 'Z'));   
+int ft_isascii(int c)
+{   
+    return (c >= 0 && c <= 127);
 }
-
-
-    
