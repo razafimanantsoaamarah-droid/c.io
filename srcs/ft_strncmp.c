21cde9e8ad4e0d905cd9da1c6aa0b1571a386833
maherraz <maherraz@student.42antananarivo.mg>
2026-01-24 16:33:08 +0300

    feat(ft_strncmp): Cette fonction permet de comparer deux chaînes de caractères et de savoir si la première est inférieure, égale ou supérieure à la seconde. Cette comparaison sera faite dans l'ordre lexicographique (et donc, en tenant compte des valeurs ASCII des différents caractères comparés). Néanmoins la comparaison se fera au maximum sur les length premiers caractères.
    En langage C, les chaînes de caractères sont qualifiées d'AZT : A Zéro Terminal. Cela signifie qu'une chaîne de caractères se termine forcément par un code ASCII nul (pouvant aussi être représenté par '\0').

---
+++ srcs/ft_strncmp.c
@@ -0,0 +1,26 @@
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maherraz <maherraz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/24 16:30:31 by maherraz          #+#    #+#             */
+/*   Updated: 2026/01/24 16:30:32 by maherraz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int ft_strncmp(const char *s1, const char *s2, unsigned int n)
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
+    {
+        if (s1[i] != s2[i])
+            return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+        i++;
+    }
+    return 0;
+}
