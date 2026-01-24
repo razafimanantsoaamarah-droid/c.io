277d96cb0e02dcdd3a51ee6544c584a953ec908c
maherraz <maherraz@student.42antananarivo.mg>
2026-01-25 02:39:19 +0300

    feat(memcpy):Cette fonction permet de copier un bloc de mémoire spécifié par le paramètre source, et dont la taille est spécifiée via le paramètre size, dans un nouvel emplacement désigné par le paramètre destination. Il est bien entendu qu'il est de votre responsabilité d'allouer suffisamment de mémoire pour le bloc de destination afin qu'il puisse contenir toutes les données. - dest : permet de spécifier l'adresse du bloc mémoire devant recevoir les données à copier. Vous devez pré-allouer ce bloc de mémoire (certainement via la fonction malloc). - src : permet de définir l'adresse du bloc de mémoire à dupliquer. -n : indique le nombre d'octets à dupliquer.

---
+++ srcs/ft_memcpy.c
@@ -0,0 +1,33 @@
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maherraz <maherraz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/25 02:18:39 by maherraz          #+#    #+#             */
+/*   Updated: 2026/01/25 02:18:40 by maherraz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stddef.h>
+void *ft_memcpy(void *dst, const void *src, unsigned int n)
+{
+    unsigned char *d;
+    const unsigned char *s;
+    unsigned int i;
+
+    if (dst == NULL && src == NULL)
+        return (NULL);
+    d = (unsigned char *)dst;
+    s = (const unsigned char *)src;
+    i = 0;
+    while (i < n)
+    {
+        d[i] = s[i];
+        i++;
+    }
+    return (dst);
+}
