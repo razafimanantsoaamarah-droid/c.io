6fc9ed952ccb89c29a1bda94bc7e7803c12d1d4c
maherraz <maherraz@student.42antananarivo.mg>
2026-01-25 02:36:58 +0300

    feat(memcmp)int memcmp( const void * pointer1, const void * pointer2, size_t size ); Cette fonction permet de comparer le contenu de deux blocs de mémoire spécifiés par les deux premiers paramètres de la fonction. Le troisième paramètre, size, permet de spécifier le nombre d'octets sur lequel comparer les deux blocs. Les octets des deux blocs sont comparés deux à deux. Tant que les deux octets comparés sont égaux, la fonction passe aux deux suivants. Si les deux octets comparés sont différents, alors la fonction renvérra lequel des deux blocs est le plus petit en termes de contenu. - s1 : permet de spécifier l'adresse du premier bloc mémoire à comparer. - s2 : permet de spécifier l'adresse du second bloc mémoire à comparer. size : indique le nombre d'octets maximal à comparer.

---
+++ srcs/ft_memcmp.c
@@ -0,0 +1,32 @@
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maherraz <maherraz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/25 02:19:34 by maherraz          #+#    #+#             */
+/*   Updated: 2026/01/25 02:19:35 by maherraz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_memcmp(const void *s1, const void *s2, unsigned int n)
+{
+    const unsigned char *str1;
+    const unsigned char *str2;
+    unsigned int i;
+
+    str1 = (const unsigned char *)s1;
+    str2 = (const unsigned char *)s2;
+    i = 0;
+    while (i < n)
+    {
+        if (str1[i] != str2[i])
+            return (str1[i] - str2[i]);
+        i++;
+    }
+    return (0);
+}
