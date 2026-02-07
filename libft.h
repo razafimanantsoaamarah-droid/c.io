abcf4f4e691b128f7c9b396ee24c0dba9105aa3b
razafimanantsoaamarah-droid <razafimanantsoaamarah@gmail.com>
2026-02-07 19:44:04 +0000

    feat():Cette fonction permet de tester si un caractère est alphabétique ou non. - Un caractère sera considéré comme alphabétique s'il est de lettres minuscules ou de lettres majuscules. - Rappel : les fonctions de cette librairie ne traitent que les caractères purs ASCII. Les lettres accentuées ne seront donc pas considérées comme des caractères alpha-numériques.

---
+++ libft.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maherraz <maherraz@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/07 19:40:11 by maherraz          #+#    #+#             */
+/*   Updated: 2026/02/07 19:41:49 by maherraz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int ft_isalpha(unsigned int c);
+int	ft_isdigit(unsigned int c);
+int	ft_isalnum(unsigned int c);
+int	ft_isascii(unsigned int c);
+int	ft_isprint(unsigned int c);
+int	ft_toupper(unsigned int c);
+int	ft_tolower(unsigned int c);
